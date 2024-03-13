e3576690ea459c0c93948cee0d82ad59a7e01a78
Ovidiu Latcu <ovidiu.latcu@gmail.com>
2024-03-13 13:58:57 +0200

    feat: add JDBC example and setup docker for Postgresql

--- src/main/java/com/olu/contentcalendar/model/Content.java
+++ src/main/java/com/olu/contentcalendar/model/Content.java
@@ -2,10 +2,13 @@ package com.olu.contentcalendar.model;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.time.LocalDateTime;
 
 public record Content(
+        @Id
         Integer id,
         @NotBlank
         String title,
